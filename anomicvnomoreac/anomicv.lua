e2db539d4c9222c101534b10dfa1533bd8a4941b
Peanutnoodlez <94880442+Peanutnoodlez@users.noreply.github.com>
2022-12-02 14:16:07 -0800

    Update anomicv.lua

--- anomicvnomoreac/anomicv.lua
+++ anomicvnomoreac/anomicv.lua
@@ -1155,9 +1155,9 @@ local function getitem()
 			game:GetService("ReplicatedStorage"):FindFirstChild("_CS.Events").LockPrinter:FireServer(v)
 			end
 			 if getCurrentVehicle() ~= nil then
-            		getCurrentVehicle():SetPrimaryPartCFrame(v.BGPart.CFrame) * CFrame.new(0,-2,0))
+            		getCurrentVehicle():SetPrimaryPartCFrame(CFrame.new(v.BGPart.CFrame)) * CFrame.new(0,-2,0))
 			else 
-			LPlayer.Character.HumanoidRootPart.CFrame = v.BGPart.CFrame      
+			LPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.BGPart.CFrame)  
 			end
 			for i = v.Properties.ItemsLeft.Value+1,1,-1 do
             game:GetService("ReplicatedStorage"):FindFirstChild("_CS.Events").DeliveryFunction:FireServer("TakeItem",v)
