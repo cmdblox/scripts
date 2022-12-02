7ab0902ca6dc80a268e0d0608fc0ada20f0b4083
Peanutnoodlez <94880442+Peanutnoodlez@users.noreply.github.com>
2022-12-01 19:24:37 -0800

    Update anomicv.lua

--- anomicvnomoreac/anomicv.lua
+++ anomicvnomoreac/anomicv.lua
@@ -1145,7 +1145,7 @@ teleSection1:addKeybind("Click TP Keybind", nil, function()
     end
 end)
 --< cmd
-testsection:addButton("Say game name",function()game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(game.Name, "All")end)
+testsection:addButton("Say Name",function()game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(tostring(game.Players.LocalPlayer.Name), "All")end)
 		
 --< teleportation
 teleSection2:addButton("Arway", function()
