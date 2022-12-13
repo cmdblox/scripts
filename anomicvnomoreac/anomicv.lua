9339393ff78b6711352916347fa6e3b7c3e9a983
Peanutnoodlez <94880442+Peanutnoodlez@users.noreply.github.com>
2022-12-13 14:10:48 -0800

    Update anomicv.lua

--- anomicvnomoreac/anomicv.lua
+++ anomicvnomoreac/anomicv.lua
@@ -1319,7 +1319,7 @@ teleSection3:addButton("Safe Spot 3", function()
 if getCurrentVehicle() ~= nil then
     getCurrentVehicle():SetPrimaryPartCFrame(CFrame.new(1370.47009, 71.7390747, 1057.67322, -0.805606365, 3.60798893e-08, -0.592451155, 9.24334884e-08, 1, -6.47903775e-08, 0.592451155, -1.06957877e-07, -0.805606365) * CFrame.new(0,5,0))
 else 
-    LPlayer.Character.HumanoidRootPart.CFrame = (CFrame.new(1370.47009, 71.7390747, 1057.67322, -0.805606365, 3.60798893e-08, -0.592451155, 9.24334884e-08, 1, -6.47903775e-08, 0.592451155, -1.06957877e-07, -0.805606365)end end)
+LPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1370.47009, 71.7390747, 1057.67322, -0.805606365, 3.60798893e-08, -0.592451155, 9.24334884e-08, 1, -6.47903775e-08, 0.592451155, -1.06957877e-07, -0.805606365) end end)
 teleSection3:addButton("Safe Spot 4", function()
 if getCurrentVehicle() ~= nil then
     getCurrentVehicle():SetPrimaryPartCFrame(CFrame.new(1370.47009, 71.7390747, 1057.67322, -0.805606365, 3.60798893e-08, -0.592451155, 9.24334884e-08, 1, -6.47903775e-08, 0.592451155, -1.06957877e-07, -0.805606365) * CFrame.new(0,5,0))
@@ -1331,7 +1331,7 @@ if getCurrentVehicle() ~= nil then
 else 
     LPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(369.557709, -1.14998794, -1313.92529, -0.276157141, -6.77183323e-08, -0.961112499, -1.02067128e-08, 1, -6.75255762e-08, 0.961112499, -8.83787088e-09, -0.276157141)      
 end end
-
+)
 -- Buy Page
 local currentTool = nil 
 local color1 = Color3.fromRGB(255,255,255)
