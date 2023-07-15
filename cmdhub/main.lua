aea090207370bd0f6826096313840919817a52b3
cmdblock <94880442+Peanutnoodlez@users.noreply.github.com>
2023-07-15 02:54:20 -0700

    Update main.lua

--- cmdhub/main.lua
+++ cmdhub/main.lua
@@ -1,5 +1,14 @@
 lib = loadstring(game:HttpGet("https://github.com/Peanutnoodlez/scripts/raw/main/cmdhub/cmdgui.lua"))()
 
+local kickphrase = getgenv().kickphrase
+
+function jsonencode(text) 
+	return game:GetService("HttpService"):JSONEncode(text)
+end
+
+function jsondecode(text) 
+	return game:GetService("HttpService"):JSONDecode(text)
+end
 
 local LPlayer = game.Players.LocalPlayer
 
@@ -12,7 +21,9 @@ local function antikick()
             mt.__namecall = function(self,...)
                 local args = {...}
                 if getnamecallmethod() == "Kick" then
-                    return nil                    
+                	if ... ~= kickphrase then
+          				return nil
+                    end
                 end
             return namecall(self,...)
         end
@@ -22,6 +33,9 @@ end
 gui = lib:new('cmd hub')
 
 lib:addbutton(gui,'antikick',antikick())
+lib:addbutton(gui,'kick self',function()
+	game.Players.LocalPlayer:Kick(kickphrase)
+end)
 lib:addbutton(gui,'print name',function()
 	print(LPlayer.Name)
 end)
