b64ccb846dba23e951d06270f91514e806a20b6b
cmdblock <94880442+Peanutnoodlez@users.noreply.github.com>
2023-07-15 02:55:40 -0700

    Update loadstring.lua

--- cmdhub/loadstring.lua
+++ cmdhub/loadstring.lua
@@ -1,1 +1,2 @@
+getgenv().kickphrase = 'selfkicked' --this bypasses antikick so be careful what you set it to
 loadstring(game:HttpGet("https://github.com/Peanutnoodlez/scripts/raw/main/cmdhub/main.lua"))()
